828c6819ba8db4e050c750c690d2a7a544e48247
Andresrodart <arodartel1601@alumno.ipn.mx>
2019-05-29 13:11:23 -0500

    Signals

---
+++ public/javascript/functions.js
@@ -0,0 +1,30 @@
+function showInput(){
+	fade(document.getElementById('main'), document.getElementById('User'));
+}
+
+function fade(element, element2Unfade) {
+	var op = 1;  // initial opacity
+	var timer = setInterval(function () {
+		if (op <= 0.1){
+			clearInterval(timer);
+			element.style.display = 'none';
+			unfade(element2Unfade);
+		}
+		element.style.opacity = op;
+		element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+		op -= op * 0.1;
+	}, 50);
+}
+
+function unfade(element) {
+	var op = 0.1;  // initial opacity
+	element.style.display = 'block';
+	var timer = setInterval(function () {
+		if (op >= 1){
+			clearInterval(timer);
+		}
+		element.style.opacity = op;
+		element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+		op += op * 0.1;
+	}, 10);
+}
